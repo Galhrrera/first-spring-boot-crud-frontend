6602bdb9298dc51976ed02ccfa297b6351ebc718
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-03-03 15:41:19 -0500

    Se crearó un formulario PersonasFrom

--- FirstCrudSpringBootApplication/src/app/app.component.ts
+++ FirstCrudSpringBootApplication/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import {EstadosService} from './services/estados/estados.service';
-import {PersonasService} from './services/personas/personas.service';
-import {PaisesService} from './services/paises/paises.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { PersonasService } from './services/personas/personas.service';
+import { PaisesService } from './services/paises/paises.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +10,22 @@ import {PaisesService} from './services/paises/paises.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'First CRUD App SpringBoot';
+
+  personasForm!: FormGroup;
 
   constructor(
-    public fb: FormBuilder;
-  ){
-
-  }
+    public fb: FormBuilder,
+    public paisesService: PaisesService,
+    public personasService: PersonasService,
+    public estadosService: EstadosService
+  ) {}
 
   ngOnInit(): void {
-      
+    this.personasForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      estado: ['', Validators.required]
+    });
   }
 }
