87125a9e0f1915012c3294770f84d89f11dd5fc4
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-03-03 15:21:58 -0500

    Se crearon los servicios estados, paises y personas. Se importaton en el app.component

--- FirstCrudSpringBootApplication/src/app/app.component.ts
+++ FirstCrudSpringBootApplication/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {EstadosService} from './services/estados/estados.service';
+import {PersonasService} from './services/personas/personas.service';
+import {PaisesService} from './services/paises/paises.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FirstCrudSpringBootApplication';
+export class AppComponent implements OnInit {
+  title = 'First CRUD App SpringBoot';
+
+  constructor(
+    public fb: FormBuilder;
+  ){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
